bb3e628e29ae0dc111d317ad66c9e7637d141da9
Ana Ferreira <morgi.chin@gmail.com>
2023-12-17 00:23:58 -0300

    add folders, db helper and Task model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:planner/pages/Tasks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -95,100 +96,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
-
-Widget Tasks(context) {
-  List<List<String>> list = [
-    ['0', 'Reunião de desenvolvimento', '10:00 - 11:00', 'Remoto'], 
-    ['1', 'Reunião de planejamento', '10:00 - 11:00', 'Remoto'], 
-    ['2', 'Reunião de suporte web', '10:00 - 11:00', 'Remoto'], 
-    ['3', 'Aula de yoga', '10:00 - 11:00', 'Presencial'], 
-    ['2', 'Live da Ismeiow', '10:00 - 11:00', 'Presencial'], 
-    ['2', 'Live da Ismeiow', '10:00 - 11:00', 'Presencial'], 
-    ['2', 'Live da Ismeiow', '10:00 - 11:00', 'Presencial'], 
-  ];
-
-  return Container(
-    decoration: const BoxDecoration(color: Colors.white, 
-      borderRadius: 
-        BorderRadius.only(
-          topLeft: Radius.circular(18),
-          topRight: Radius.circular(18),),
-      ),
-    child: Center (
-      child: ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(left:18, right:18, top:18),
-            child: Task(list[index], context),
-          );
-        }),
-    ),
-  );
-}
-
-Widget Task(list, context) {
-  Color? colour;
-  switch (list[0]) {
-    case '0':
-      colour = Theme.of(context).colorScheme.primary;
-      break;
-    case '1':
-      colour = Theme.of(context).colorScheme.secondary;
-      break;
-    case '2':
-      colour = Theme.of(context).colorScheme.tertiary;
-      break;
-    case '3':
-      colour = Theme.of(context).colorScheme.surface;
-      break;
-    default:
-  }
-
-  return Column(
-    children: [
-      Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Color.fromRGBO(247, 247, 247, 1),
-          // border: Border(left: BorderSide(color: Colors.black, width: 4)),
-        ),
-        child: Row(
-          children: [
-            Container(
-              width: 8,
-              height: 110,
-              decoration: BoxDecoration(
-                color: colour,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(8), 
-                  bottomLeft: Radius.circular(8), ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 24),
-              child: 
-                  SizedBox (
-                    height: 73,
-                    width: MediaQuery.of(context).size.width*0.8,
-                    child: ListView.builder(
-                      itemCount: list.length,
-                      itemBuilder: (c, index) {
-                        if (index != 0) {
-                          if (index == 1) {
-                            return Text( list[index], 
-                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),);
-                          }
-                          return Text(list[index]);
-                        }
-                        return const SizedBox.shrink();
-                      },
-                    ),
-                  ),)
-                ],
-              ),
-            ),
-          ],
-  );
 }
