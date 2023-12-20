c5f34c00f287f87703f9b5e9ef6c785e62aacdc1
Ana Ferreira <morgi.chin@gmail.com>
2023-12-20 20:44:49 -0300

    fix buggy merge

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:planner/pages/NewTaskBoard.dart';
 import 'package:planner/pages/widgets/NewEvent.dart';
 import 'package:planner/pages/widgets/Profile.dart';
 import 'package:planner/pages/widgets/Next.dart';
@@ -34,7 +34,7 @@ class _HomePageState extends State<HomePage> {
     ];
 
     bool isVisible = true;
-    if (_selectedIndex == 3) {
+    if (_selectedIndex >= 2) {
       isVisible = false;
     } else
       isVisible = true;
@@ -99,9 +99,12 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: Visibility(
         visible: isVisible,
         child: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => EventCreator()));
+          onPressed: () { 
+            if (_selectedIndex == 0) {
+              Navigator.push(context,  MaterialPageRoute(builder: (context) => BoardCreator()));
+            } else {
+              Navigator.push(context,  MaterialPageRoute(builder: (context) => EventCreator()));
+            }
           },
           tooltip: 'Add new event',
           child: const Icon(Icons.add),
