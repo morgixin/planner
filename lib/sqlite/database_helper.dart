8d82f29d2fddb364b73675dc185cb5f4da5af771
marws <mariacto@id.uff.br>
2023-12-20 07:41:32 -0300

    Update database_helper.dart

--- lib/sqlite/database_helper.dart
+++ lib/sqlite/database_helper.dart
@@ -24,8 +24,7 @@ class DatabaseHelper {
 
     final path = join(databasePath, "data.db");
 
-    Database db = await openDatabase(path, version: 1, onCreate: _onCreateDb);
-    return db;
+    return await openDatabase(path, version: 1, onCreate: _onCreateDb);
   }
 
   void _onCreateDb(Database db, int version) async {
