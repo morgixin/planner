0815fbb5746b1fab3e065ce537768daec4c92bd3
dabmenez <dbmenezes@id.uff.br>
2023-12-20 18:18:20 -0300

    taskboard.dart

---
+++ lib/model/TaskBoard.dart
@@ -0,0 +1,27 @@
+class TaskBoard {
+  int id;
+  String name;
+  String color;
+
+  TaskBoard({
+    required this.id,
+    required this.name,
+    required this.color,
+  });
+
+  factory TaskBoard.fromMap(Map<String, dynamic> map) {
+    return TaskBoard(
+      id: map['id'],
+      name: map['name'],
+      color: map['color'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'color': color,
+    };
+  }
+}
